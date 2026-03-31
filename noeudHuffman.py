14ad41780683ce88818b64bf3f541cf1bfbe98b7
d_assane <assane.diebate@etu.u-pec.fr>
2026-03-31 15:23:28 +0200

    [Classe noeudHuffman] : correction getters et setters, début de l'implémentation de la méthode construction arbre huffman

--- noeudHuffman.py
+++ noeudHuffman.py
@@ -7,19 +7,30 @@ class NoeudHuffman(NoeudBinaire):
 
     # Getters
     def get_caracteres(self):
-        return self.caracteres
+        return self.valeurs[0] # premier element du tuple (caracteres, poids)
     
     def get_poids(self):
-        return self.poids
-    
+        return self.valeurs[1] # deuxième element du tuple (caracteres, poids)
+
     # Setters
     def set_caracteres(self, caracteres):
-        self.caracteres = caracteres
+        self.valeur = (caracteres, self.valeur[1])
 
     def set_poids(self, poids): 
-        self.poids = poids
+        self.valeur = (self.valeur[0], poids)
 
     # Méthode permettant de construire l'arbre de Huffman à partir d'une chaine de caractères 
-    def construire_arbre_huffman(self):
-        
-        pass
+    def construire_arbre_huffman(self, caracteres):
+        # On commence par créer la liste L dont les éléments sont les couples (caractere, effectif du caractere)
+        effectif_dictionnaire = {}                    
+        for c in caracteres:
+            if c in effectif_dictionnaire:
+                effectif_dictionnaire[c] += 1
+            else:
+                effectif_dictionnaire[c] = 1
+
+        L = []
+        for c in effectif_dictionnaire:
+            L.append((c, effectif_dictionnaire[c]))
+
+        return L
