1cc035f3be639ffd8700074136b924d3506f3182
Arthur_Sivault <arthur.sivault-le-morellec@etu.u-pec.fr>
2026-04-07 10:52:06 +0200

    Ajout des tests

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from NoeudBinaire import NoeudBinaire
 from NoeudHuffman import NoeudHuffman
-"""
+
 g = NoeudBinaire('G', None, None) # Arbre de valeur 'G', sans sous-arbre (feuille)
 # Arbre de valeur 'F'. Sous-arbre gauche : g. Pas sous-arbre droit.
 f = NoeudBinaire('F', g, None)
@@ -15,13 +15,11 @@ b = NoeudBinaire('B', c, d)
 # Arbre de valeur 'A', sous-arbre gauche : b. Sous-arbre droit : e.
 a = NoeudBinaire('A', b, e)
 mon_arbre = a
-
+print("Affichage NoeudBinaire : ")
 print(mon_arbre)
-"""
-
 
 s = "bonjourbonsoir"
-
+print("Affichage NoeudHuffman : ")
 arbre = NoeudHuffman.construire_arbre(s)
 print(arbre)
 
