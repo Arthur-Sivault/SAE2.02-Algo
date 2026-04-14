781fd3b6a75d92e5ee3a1598bf3b28f61b4985c1
valentincarp <valentin.carpinteiro.lericq@gmail.com>
2026-04-14 12:42:48 +0200

    Fin du système de recherche des fichiers

--- main.py
+++ main.py
@@ -22,3 +22,23 @@ for fichier in os.listdir(dossier_input):
             arbre = NoeudHuffman.construire_arbre(contenuTxt)
             codes = arbre.generer_codes()
             chaine_compressee = NoeudHuffman.compression(contenuTxt, codes)
+
+            
+            # Affichage
+            print("Binaire ASCII :", binaire_initial[:100], "...")
+            print("Compressé :", chaine_compressee[:100], "...")
+
+            # Tailles de la chaine initiale et compressée
+            taille_initiale = len(binaire_initial)
+            taille_compressee = len(chaine_compressee)
+
+            # Calcul et affichage du taux de compression
+            taux = (1 - taille_compressee / taille_initiale) * 100
+
+            print(f"Taille initiale : {taille_initiale} bits")
+            print(f"Taille compressée : {taille_compressee} bits")
+            print(f"Taux : {taux:.2f}%")
+
+            # Décompression et vérification
+            decomp = NoeudHuffman.decompression(chaine_compressee, arbre)
+            print("Décompression OK" if decomp == contenuTxt else "Erreur")
